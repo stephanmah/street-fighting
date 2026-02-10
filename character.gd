268e89fc1a75d32c7105c1818ad7826a97f6d520
Stephan Mah <35859912+stephanmah@users.noreply.github.com>
2026-02-10 13:36:46 -0500

    Refactor player movement and animation
    Split player logic into input, movement and animation handlers; add a State enum and set state based on velocity. Use AnimationPlayer to play "idle" and "walk" animations. Update player.tscn walk animation: set length to 0.8 and remove the extra final key/time to match frame values.

--- character.gd
+++ character.gd
@@ -4,7 +4,30 @@ extends CharacterBody2D
 @export var damage : int
 @export var speed : float
 
+@onready var animation_player := $AnimationPlayer
+
+enum State {IDLE,WALK}
+
+var state = State.IDLE
+
 func _process(delta: float) -> void:
-	var direction := Input.get_vector("ui_left","ui_right","ui_up","ui_down")
-	position += direction * delta * speed
+	handle_input()
+	handle_movement()
+	handle_animation()
 	move_and_slide()
+
+func handle_movement():
+	if velocity.length() == 0:
+		state = State.IDLE
+	else:
+		state = State.WALK
+		
+func handle_input() -> void:
+	var direction := Input.get_vector("ui_left","ui_right","ui_up","ui_down")
+	velocity = direction  * speed
+	
+func handle_animation() -> void:
+	if state == State.IDLE:
+		animation_player.play("idle")
+	elif state == State.WALK:
+		animation_player.play("walk")
